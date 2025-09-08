28b5746bf959d82a4157596e0adba1dc25c46540
Caiohssilva <caiohssilva01@gmail.com>
2025-09-08 10:35:25 -0300

    Update App.tsx0

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import LoginPage from './features/auth/LoginPage';
+import AuthCallback from './pages/AuthCallback';
+
 export default function App() {
+  if (window.location.pathname.startsWith('/auth/callback')) {
+    return <AuthCallback />;
+  }
   return <LoginPage />;
 }
