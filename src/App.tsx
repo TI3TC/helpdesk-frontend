a6cbf930c218e71f1c82a28510005058947a8ce5
Caiohssilva <caiohssilva01@gmail.com>
2025-09-11 10:42:06 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import LoginPage from './features/auth/LoginPage';
-import AuthCallback from './pages/AuthCallback';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import Tickets from "./pages/Tickets";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import { AuthProvider } from "./store/auth";
+
+
+const router = createBrowserRouter([
+{ path: "/login", element: <Login /> },
+{
+path: "/",
+element: <ProtectedRoute />,
+children: [
+{ path: "/tickets", element: <Tickets /> }
+]
+}
+]);
+
 
 export default function App() {
-  if (window.location.pathname.startsWith('/auth/callback')) {
-    return <AuthCallback />;
-  }
-  return <LoginPage />;
+return (
+<AuthProvider>
+<RouterProvider router={router} />
+</AuthProvider>
+);
 }
