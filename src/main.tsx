cb7809c0c1e725efe7f32bfad5ba993eef902b74
Caiohssilva <caiohssilva01@gmail.com>
2025-09-15 16:11:08 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// main.tsx (ou App.tsx)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ensureMsalInitialized } from './auth/msal';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ensureMsalInitialized().then(() => {
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
